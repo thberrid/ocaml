3bcddf4f580b9fad59e71399109260cd67d3dda1
thierry <thberrid@student.42.fr>
2021-03-17 00:03:43 +0100

    rot_n int > string > string

---
+++ D00/ex08/ft_rot_n.ml
@@ -0,0 +1,20 @@
+let ft_rot_n n s =
+	let f c =
+		let rot_from start = 
+			char_of_int ((int_of_char start) + (((int_of_char c) - (int_of_char start) + n) mod 26))
+		in
+		if (c >= 'a' && c <= 'z') then
+			rot_from 'a'
+		else if (c >= 'A' && c <= 'Z') then
+			rot_from 'A'
+		else c
+in String.map f s
+
+let main () =
+  print_endline (ft_rot_n 0 "az_0789_AZ") ;
+  print_endline (ft_rot_n 1 "az_0789_AZ") ;
+  print_endline (ft_rot_n 13 "az_0789_AZ") ;
+  print_endline (ft_rot_n 52 "az_0789_AZ") ;
+  print_endline (ft_rot_n 26 "")
+
+let () = main ()
