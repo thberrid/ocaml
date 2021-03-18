10ba56857ad25fe675c0d5ef7841e838aee528cd
thierry <thberrid@student.42.fr>
2021-03-18 21:32:45 +0100

    iter (int > int) int > int > int

--- D01/ex06/iter.ml
+++ D01/ex06/iter.ml
@@ -1,8 +1,7 @@
 let rec iter f x n =
 		if n < 0 then ~-1
 		else if n = 0 then x
-		else if n = 1 then f x
-		else iter f (iter f x 1) (n - 1)
+		else iter f (f x) (n - 1)
 		
 let main () = 
 	print_int (iter (fun x -> x * x) 2 4) ; print_endline "" ;
