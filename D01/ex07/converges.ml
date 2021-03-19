5bab4ec5ce5fd52feb4088d8ec1aa5b08fe45d6e
thierry <thberrid@student.42.fr>
2021-03-19 11:52:08 +0100

    converges (fixed point) ('a > 'a) > 'a > int > bool

--- D01/ex07/converges.ml
+++ D01/ex07/converges.ml
@@ -1,12 +1,15 @@
 let rec converges f x n =
-	if n <= 0 then false
+	if n < 0 then false
+	else if n = 0 && x <> 0 then false
+	else if n <= 0 && x = 0 then true
 	else if x = f(x) then true
 	else converges f (f x) (n - 1)
 
 
 let main () = 
+	if (converges (( * ) 2) 2 5 = true) then print_endline "TRUE" else print_endline "FALSE" ;
 	if (converges (fun x -> x) 2 50 = true) then print_endline "TRUE" else print_endline "FALSE" ;
-	if (converges (fun x -> x / 2) 2 3  = true) then print_endline "TRUE" else print_endline "FALSE" ;
-	if (converges (fun x -> x / 2) 2 2 = true) then print_endline "TRUE" else print_endline "FALSE"
+	if (converges (fun x -> x / 2 + 1) 2 3  = true) then print_endline "TRUE" else print_endline "FALSE" ;
+	if (converges (fun x -> x / 2) 5 1 = true) then print_endline "TRUE" else print_endline "FALSE"
 
 let () = main ()
