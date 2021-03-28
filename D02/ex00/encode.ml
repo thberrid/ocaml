15159c7834c068b3548dda6a5694e494c25f69a1
thierry <thberrid@student.42.fr>
2021-03-28 15:06:17 +0200

    encode 'a list > (int * 'a) list

---
+++ D02/ex00/encode.ml
@@ -0,0 +1,32 @@
+let encode list = 
+	let rec loop inputlist outputlist count = match inputlist with 
+(*	| tete::queue	->	if tete = queue then loop queue ([(count, queue)] @ output) 1
+											else loop queue output (count + 1)
+		| head::tail when ([head] <> [tail]) -> loop queue output (count + 1)
+		| head::tail when head = tail -> loop queue ([(count, queue)] @ output) 1
+	*)
+		| [] -> []
+		| head :: (tail :: _ as next)	-> if head = tail then loop next outputlist (count + 1)
+																		else loop next (outputlist @ [(count, head)]) 1 
+		| [x]														-> outputlist @ [(count, x)] 
+	in loop list [] 1
+
+
+(* tests *)
+
+let rec print_charlist list = match list with
+	| tete::queue -> print_int (fst tete) ; print_char ' ' ; print_char (snd tete) ; print_endline "" ; print_charlist queue
+	| [] 					-> print_endline ""
+
+let rec print_intlist list = match list with
+	| tete::queue -> print_int (fst tete) ; print_char ' ' ; print_int (snd tete) ; print_endline "" ; print_intlist queue
+	| [] 					-> print_endline ""
+
+let list1 = 4 :: 5 :: 6 :: 9 :: 9 :: 9 :: 9 :: []
+let list_char = '0' :: 'a' :: 'a' :: 'a' :: 'b' :: 'a' :: []
+
+let main () = 
+	print_charlist (encode list_char) ;
+	print_intlist (encode list1)
+
+let () = main ()
